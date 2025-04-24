4c46209e200720bc6f487525a3254010ee52d0e1
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-04-24 14:53:59 +0200

    adding more test cases

--- analyzer/testdata/src/simple/special-cases.go
+++ analyzer/testdata/src/simple/special-cases.go
@@ -8,3 +8,13 @@ func myFunction() {
 		time.Time // want `embedded types should be listed before non embedded types`
 	}
 }
+
+type DeclaredSameLineNoSpace struct {
+	time.Time // want `there must be an empty line separating embedded fields from regular fields`
+	lat, long int
+}
+
+type DeclaredSameLineEmbeddedLast struct {
+	lat, long int
+	time.Time // want `embedded types should be listed before non embedded types`
+}
