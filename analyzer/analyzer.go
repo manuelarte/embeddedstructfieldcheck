097abe3ec78215b7df1b6a1ebcae95b5ea5581ee
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-05-15 20:22:26 +0200

    fixing lint issue

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -29,7 +29,8 @@ func NewAnalyzer() *analysis.Analyzer {
 		Requires: []*analysis.Analyzer{inspect.Analyzer},
 	}
 
-	a.Flags.BoolVar(&forbidMutex, ForbidMutexName, false, "Checks that sync.Mutex and sync.RWMutex are not used as an embedded fields.")
+	a.Flags.BoolVar(&forbidMutex, ForbidMutexName, false,
+		"Checks that sync.Mutex and sync.RWMutex are not used as an embedded fields.")
 
 	return a
 }
